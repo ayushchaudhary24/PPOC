c9416e31285a26149ed81ec5bba59b7148b55d15
ayushchaudhary24 <ayushc24@iitk.ac.in>
2025-05-13 15:24:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/about-us")
+def about():
+    return render_template('about-us.html')
+
+@app.route("/events")
+def events():
+    return render_template('events.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
